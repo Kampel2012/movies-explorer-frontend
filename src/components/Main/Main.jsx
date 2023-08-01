4d0a9f102bf7678907a9ab5055714d7f4c88ff53
Anton Glazunov <kampel2012@yandex.ru>
2023-08-01 20:51:43 +0500

    вёрстка секции технологии

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,13 @@
 import AboutProject from './AboutProject/AboutProject';
 import Promo from './Promo/Promo';
+import Techs from './Techs/Techs';
 
 function Main(props) {
   return (
     <>
       <Promo />
       <AboutProject />
+      <Techs />
     </>
   );
 }
---
+++ src/components/Main/Techs/Tech/Tech.jsx
@@ -0,0 +1,7 @@
+import './Tech.css';
+
+function Tech({ text }) {
+  return <div className="tech">{text}</div>;
+}
+
+export default Tech;
