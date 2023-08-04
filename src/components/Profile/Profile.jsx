c48596d6fc08a8506352b2b0bd43a1be4e3f6cb7
Anton Glazunov <kampel2012@yandex.ru>
2023-08-04 16:48:28 +0500

    Вёрстка профиля

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,37 @@
-function Profile(props) {
-  return <div>Profile</div>;
+import Header from '../Header/Header';
+import './Profile.css';
+
+function Profile({ name = 'Виталий', email = 'pochta@yandex.ru' }) {
+  return (
+    <>
+      <Header isAuth={true} />
+      <section className="profile">
+        <div className="profile__container">
+          <h1 className="profile__title">{`Привет, ${name}!`}</h1>
+          <div className="profile__info">
+            <p className="profile__info-field">
+              <span className="profile__tag">Имя</span>
+              <span className="profile__tag">{name}</span>
+            </p>
+            <p className="profile__info-field">
+              <span className="profile__tag">E-mail</span>
+              <span className="profile__tag">{email}</span>
+            </p>
+          </div>
+          <div className="profile__wrapper">
+            <button type="button" className="profile__btn">
+              Редактировать
+            </button>
+            <button
+              type="button"
+              className="profile__btn profile__btn_type_exit">
+              Выйти из аккаунта
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Profile;
