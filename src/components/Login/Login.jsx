6d4639f01b7fd7bd17cd2db834940a7b681842c8
Anton Glazunov <kampel2012@yandex.ru>
2023-08-09 18:08:28 +0500

    style добавить ховер

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,47 +5,47 @@ import { Link } from 'react-router-dom';
 function Login(props) {
   return (
     <section className="login">
-      <form className="login-container">
-        <div className="login-logo">
+      <form className="login__container">
+        <div className="login__logo">
           <Link to={'/'}>
-            <img src={logo} alt="Логотип" className="login-logo__image" />
+            <img src={logo} alt="Логотип" className="login__logo-image" />
           </Link>
         </div>
-        <h2 className="login-form-title">Рады видеть!</h2>
-        <div className="login-form-group">
-          <label className="login-form-label" htmlFor="email">
+        <h2 className="login__form-title">Рады видеть!</h2>
+        <div className="login__form-group">
+          <label className="login__form-label" htmlFor="email">
             E-mail
           </label>
           <input
-            className="login-form-input"
+            className="login__form-input"
             type="email"
             name="email"
             autoComplete="off"
             minLength={2}
             maxLength={30}
           />
-          <span className="login-error-message"></span>
+          <span className="login__error-message"></span>
         </div>
-        <div className="login-form-group">
-          <label className="login-form-label" htmlFor="password">
+        <div className="login__form-group">
+          <label className="login__form-label" htmlFor="password">
             Пароль
           </label>
           <input
-            className="login-form-input login-form-input_type_error"
+            className="login__form-input login__form-input_type_error"
             type="password"
             name="password"
             autoComplete="off"
             minLength={2}
             maxLength={30}
           />
-          <span className="login-error-message">Что-то пошло не так...</span>
+          <span className="login__error-message">Что-то пошло не так...</span>
         </div>
-        <button type="submit" className="login-button">
+        <button type="submit" className="login__button">
           Войти
         </button>
-        <p className="login-question">
+        <p className="login__question">
           Ещё не зарегистрированы?{' '}
-          <Link to={'/signup'} className="login-link">
+          <Link to={'/signup'} className="login__link">
             Регистрация
           </Link>
         </p>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,61 +5,63 @@ import { Link } from 'react-router-dom';
 function Register(props) {
   return (
     <section className="register">
-      <form className="register-container">
-        <div className="register-logo">
+      <form className="register__container">
+        <div className="register__logo">
           <Link to={'/'}>
-            <img src={logo} alt="Логотип" className="register-logo__image" />
+            <img src={logo} alt="Логотип" className="register__logo-image" />
           </Link>
         </div>
-        <h2 className="register-form-title">Добро пожаловать!</h2>
-        <div className="register-form-group">
-          <label className="register-form-label" htmlFor="name">
+        <h2 className="register__form-title">Добро пожаловать!</h2>
+        <div className="register__form-group">
+          <label className="register__form-label" htmlFor="name">
             Имя
           </label>
           <input
-            className="register-form-input"
+            className="register__form-input"
             type="text"
             name="name"
             autoComplete="off"
             minLength={2}
             maxLength={30}
           />
-          <span className="register-error-message"></span>
+          <span className="register__error-message"></span>
         </div>
-        <div className="register-form-group">
-          <label className="register-form-label" htmlFor="email">
+        <div className="register__form-group">
+          <label className="register__form-label" htmlFor="email">
             E-mail
           </label>
           <input
-            className="register-form-input"
+            className="register__form-input"
             type="email"
             name="email"
             autoComplete="off"
             minLength={2}
             maxLength={30}
           />
-          <span className="register-error-message"></span>
+          <span className="register__error-message"></span>
         </div>
-        <div className="register-form-group">
-          <label className="register-form-label" htmlFor="password">
+        <div className="register__form-group">
+          <label className="register__form-label" htmlFor="password">
             Пароль
           </label>
           <input
-            className="register-form-input register-form-input_type_error"
+            className="register__form-input register__form-input_type_error"
             type="password"
             name="password"
             autoComplete="off"
             minLength={2}
             maxLength={30}
           />
-          <span className="register-error-message">Что-то пошло не так...</span>
+          <span className="register__error-message">
+            Что-то пошло не так...
+          </span>
         </div>
-        <button type="submit" className="register-button">
+        <button type="submit" className="register__button">
           Зарегистрироваться
         </button>
-        <p className="register-question">
+        <p className="register__question">
           Уже зарегистрированы?{' '}
-          <Link to={'/signin'} className="register-link">
+          <Link to={'/signin'} className="register__link">
             Войти
           </Link>
         </p>
